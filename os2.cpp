02bd390fe944567da2888ad7b18269cd34550e31
niranjan0103 <49971548+niranjan0103@users.noreply.github.com>
2019-04-25 01:36:34 -0700

    Update os2.cpp

--- os2.cpp
+++ os2.cpp
@@ -1,6 +1,6 @@
 // C++ program to illustrate Banker's Algorithm ...
 #include<iostream> 
-using namespace std
+using namespace std;
 
 const int P = 5;
 
@@ -29,7 +29,7 @@ bool isSafe(int processes[], int avail[], int maxm[][R],
 	calculateNeed(need, maxm, allot); 
 
 	
-	bool finish[P] = {0}; 
+	bool finish[P] = {0}
 
 	
 	int safeSeq[P]; 
